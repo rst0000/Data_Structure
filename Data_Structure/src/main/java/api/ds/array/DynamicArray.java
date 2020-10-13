640d1e0697eaf1c0747574eed055733a54d68200
dev2 <dev2@outlook.com>
2020-10-13 11:03:29 +0530

    Modify DynamicArray.java

--- Data_Structure/src/main/java/api/ds/array/DynamicArray.java
+++ Data_Structure/src/main/java/api/ds/array/DynamicArray.java
@@ -1,5 +1,10 @@
 package api.ds.array;
 
 public class DynamicArray {
+	
+	public static void main(String[] args) {
+		DynamicArray darr=new DynamicArray();
+		System.out.println(darr.getClass());
+	}
 
 }
